291722cac265978ea0ea79be84e77811c7a4b225
Kim Eriksson <kimeriksson@Kims-MacBook-Pro.local>
2018-02-22 15:39:47 +0100

    connected firebase and push notes via form submit

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase';
+
+var config = {
+  apiKey: "AIzaSyCqmNCsvxfd1RPV3ZdX8-OCPJ9oF7FUME4",
+  authDomain: "door-key-d46a8.firebaseapp.com",
+  databaseURL: "https://door-key-d46a8.firebaseio.com",
+  projectId: "door-key-d46a8",
+  storageBucket: "door-key-d46a8.appspot.com",
+  messagingSenderId: "170553132043"
+};
+firebase.initializeApp(config);
+export const database = firebase.database().ref('/notes')
